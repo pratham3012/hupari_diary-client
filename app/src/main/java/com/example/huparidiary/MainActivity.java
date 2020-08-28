cdb1d616ac5b318302e4df02b2dbc626198de97e
anfaas1618 <ishad0911@gmail.com>
2020-08-28 16:07:05 +0530

    added restApi to recycler view

--- app/src/main/java/com/example/huparidiary/MainActivity.java
+++ app/src/main/java/com/example/huparidiary/MainActivity.java
@@ -6,20 +6,31 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private Toolbar toolbar;
     private RecyclerView.LayoutManager layoutManager;
-
+    String [] myDataset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
+myDataset=new String[100];
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true);
@@ -33,13 +44,47 @@ public class MainActivity extends AppCompatActivity {
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        Log.i("TAG", "onCreate: ");
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url="https://mibtechnologies.in/hupariapp/index.php";
+
+        StringRequest request=new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+//                GsonBuilder gsonBuilder=new GsonBuilder();
+//                Gson gson =gsonBuilder.create();
+//                CategoryJson categoryJson=gson.fromJson(response,CategoryJson.class);
+//                Log.d("TAG", "onResponse: "+categoryJson.getName());
+//                Log.d("TAG", "onResponse: "+categoryJson.getUid().trim());
+                GsonBuilder builder=new GsonBuilder();
+
+                Gson gson1= builder.create();
+                int i=0;
+                CategoryJson[] categoryJsons=  gson1.fromJson(response,CategoryJson[].class);
+                for (CategoryJson categoryJson1: categoryJsons)
+                {
+                    Log.i("TAG", "onResponse: "+categoryJson1.getName());
+                    myDataset[i]=categoryJson1.getName();
+                    i++;
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", "onErrorResponse:",error );
+            }
+        });
+        mAdapter = new CatAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+        queue.add(request);
 
         // specify an adapter (see also next example)
-        String [] myDataset={
-                "anfaas","amaan","rayyam"
-        };
-                mAdapter = new CatAdapter(myDataset);
-        recyclerView.setAdapter(mAdapter);
+
+
 
     }
     @Override
